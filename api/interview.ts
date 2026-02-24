d65e206f085cd163c258248a341e1d5bd9343e37
KlayPeter <kt_mmxyy2377@qq.com>
2026-02-24 09:56:30 +0800

    chore: 删除无用代码整理代码结构

--- api/interview.ts
+++
@@ -1,9 +0,0 @@
-import api from '@/lib/request';
-
-export const startInterviewAPI = (data: any) => {
-  return api.post('/interview/start', data);
-};
-
-export const getInterviewHistoryAPI = () => {
-  return api.get('/interview/history');
-};
